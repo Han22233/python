c7fa11430d1ae0d1195e075eee56280e6370d5e9
sunghan <107450674+Han22233@users.noreply.github.com>
2023-03-21 13:00:54 +0900

    Add files via upload

---
+++ ch2/2_1.py
@@ -0,0 +1,26 @@
+# 2_1.py
+# 동양미래대 
+# 파이썬 프로그래밍 
+'''
+이것은
+여러줄로 이루어진
+주석입니다
+'''
+print("Hello 'World!'")
+print('Hello "World!"')
+print(type(3))
+print(3)
+print(type(1.1))
+print(1.1)
+
+print("파이썬 " + "만세")
+print("a"+"b"+"c"+"d")
+print("파이썬 !!!" * 3) #문자열 3번 반복, 숫자만 곱할 수 있음
+print(type("Hello"))
+ 
+print(1,2,-5,3.14,2.71828)
+print('Hi','Python')
+
+print('23000원은','5000원 ?개','1000원 ?개')
+print('5000원',23000//5000,'개')
+print('1000원',(23000%5000)//1000,'개')
---
+++ ch2/2_2.py
@@ -0,0 +1,4 @@
+print(eval('3+15/2'))
+print(eval('4*3%5'))
+print(eval('3*-2**3'))
+print(eval('"java"*3'))
